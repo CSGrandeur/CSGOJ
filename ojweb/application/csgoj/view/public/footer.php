9c145002e0dc3a60e88a6dfeb15348982b577f18
csgrandeur <csgrandeur@gmail.com>
2023-10-29 23:21:04 +0800

    fix: OJ repository url

--- ojweb/application/csgoj/view/public/footer.php
+++ ojweb/application/csgoj/view/public/footer.php
@@ -4,7 +4,7 @@
         <a href="#">Back to top</a>
     </p>
     <p >Copyright © 2016-<?php echo date('Y'); ?> CSGrandeur. All Rights Reserved. <a href=https://beian.miit.gov.cn/ target="_blank">{$ICP_RECORD}</a><br/>
-        Designer & Developer : <a href="http://blog.csgrandeur.cn" target="_blank">CSGrandeur</a>. <a href="git@github.com:CSGrandeur/CSGOJ.git" target="_blank">CSGOJ-一站式XCPC比赛系统 </a>.<br/>
+        Designer & Developer : <a href="http://blog.csgrandeur.cn" target="_blank">CSGrandeur</a>. <a href="https://github.com/CSGrandeur/CSGOJ" target="_blank">CSGOJ-一站式XCPC比赛系统 </a>.<br/>
 
     </p>
     {if isset($GA_CODE) && $GA_CODE != false}
